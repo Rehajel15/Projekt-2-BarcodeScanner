e9c6956c4627c60a175ffa6e085b104b52398dcb
Rehajel <143653636+Rehajel15@users.noreply.github.com>
2024-07-17 17:56:47 +0200

    Finished the project

--- takePicture.py
+++ takePicture.py
@@ -11,38 +11,77 @@ root.title("Barcodescanner")
 lmain = tk.Label(root)
 lmain.pack(fill=tk.BOTH, expand=True)
 
+def update_frame():
+    ret, frame = cap.read()
+    if ret:
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        lmain.imgtk = imgtk
+        lmain.configure(image=imgtk)
+    lmain.after(10, update_frame)
+
+def switchToCam():
+    global lmain 
+    for widget in root.winfo_children():
+        widget.destroy()
+    
+    lmain = tk.Label(root)
+    lmain.pack(fill=tk.BOTH, expand=True)
+    take_photo_button = tk.Button(root, text="Überprüfe QR Code", command=take_photo)
+    take_photo_button.pack(pady=20)
+
+    update_frame()
 
 def displayData(data):
     for widget in root.winfo_children():
         widget.destroy()
 
-    tk.Label(root, text=f"ID: {data['code']}").pack()
-    tk.Label(root, text=f"Produktname: {data['product']['generic_name_de']}").pack()
-    tk.Label(root, text=f"Nahrungsgruppe(n): {data['product']['food_groups']}").pack()
-    tk.Label(root, text=f"Kategorie(n): {data['product']['categories']}").pack()
+    take_another_photo_button = tk.Button(root, text="Weiteres Produkt scannen", command=switchToCam)
+    take_another_photo_button.pack(pady=20)
 
-    
+    for key, value in data.items():
+        label = tk.Label(root, text=f"{key}: {value}", justify='left')
+        label.pack(padx=10, pady=10)
 
 def getData(barcodeDataCode):
-    response = requests.get(f'https://world.openfoodfacts.org/api/v2/product/{barcodeDataCode}.json')
-
-    if response.status_code == 200:
-        try:
-            data = response.json()
-            displayData(data)
-        except ValueError:
-            print("Fehler beim Dekodieren der JSON-Daten")
-    else:
-        print(f"HTTP Fehler: {response.status_code}")
+    try:
+        response = requests.get(f'https://world.openfoodfacts.org/api/v2/product/{barcodeDataCode}.json')
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx)
+    except requests.RequestException as e:
+        messagebox.showerror("Fehler", f"HTTP Fehler: {str(e)}")
         return None
+
+    try:
+        data = response.json()
+    except ValueError:
+        messagebox.showerror("Fehler", "Fehler beim Dekodieren der JSON-Daten")
+        return None
+
+    product_data = data.get('product', {})
+
+    showed_data = {
+        "ID": data.get('code', 'Keine Daten'),
+        "Produktname": product_data.get('generic_name_de', 'Keine Daten'),
+        "Marke(n)": product_data.get('brands', 'Keine Daten'),
+        "Nutri-Score Note": product_data.get('nutriscore_grade', 'Keine Daten'),
+        "Nutri-Score Punkte": product_data.get('nutriscore_score', 'Keine Daten'),
+        "In folgenden Ländern verfügbar": product_data.get('countries', 'Keine Daten'),
+        "Nahrungsgruppe(n)": product_data.get('food_groups', 'Keine Daten'),
+        "Kategorie(n)": product_data.get('categories', 'Keine Daten'),
+        "Zutaten": product_data.get('ingredients_text_de', 'Keine Daten'),
+        "Beschriftungen": product_data.get('labels', 'Keine Daten')
+    }
+
+    displayData(showed_data)
 
 def BarcodeReader(image):
     img = cv2.imread(image)
 
     detectedBarcodes = decode(img)
 
-    if not detectedBarcodes: 
-        print("Barcode Not Detected or your barcode is blank/corrupted!") 
+    if not detectedBarcodes:
+        messagebox.showerror("Fehler", "Barcode wurde nicht erkannt oder ist leer/beschädigt!")
     else: 
         for barcode in detectedBarcodes:   
             # Locate the barcode position in image 
@@ -57,8 +96,6 @@ def BarcodeReader(image):
             barcodeDataCode = barcode.data.decode("utf-8")
               
             if barcode.data!="": 
-                print(barcodeDataCode) 
-                # print(barcode.type) 
                 getData(barcodeDataCode)
 
 def take_photo():
@@ -68,35 +105,21 @@ def take_photo():
 
     ret, frame = cap.read()
     if ret:
-        # Speichere das Bild als picture.png
         cv2.imwrite("picture.png", frame)
         BarcodeReader("picture.png")
-       # messagebox.showinfo("Erfolg", "Foto wurde gespeichert als picture.png")
     else:
         messagebox.showerror("Fehler", "Konnte kein Bild aufnehmen!")
 
-def update_frame():
-    ret, frame = cap.read()
-    if ret:
-        # Konvertiere das Bild in ein Format, das tkinter anzeigen kann
-        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-        img = Image.fromarray(cv2image)
-        imgtk = ImageTk.PhotoImage(image=img)
-        lmain.imgtk = imgtk
-        lmain.configure(image=imgtk)
-    lmain.after(10, update_frame)
-
-# Zugriff auf die Kamera
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     messagebox.showerror("Fehler", "Kamera konnte nicht geöffnet werden!")
     cap.release()
     exit()
 
-# GUI erstellen
+
 take_photo_button = tk.Button(root, text="Check QR Code", command=take_photo)
 take_photo_button.pack(pady=20)
-update_frame()  # Starte den Kamera-Feed
+update_frame()
 root.mainloop()
 
 cap.release()
